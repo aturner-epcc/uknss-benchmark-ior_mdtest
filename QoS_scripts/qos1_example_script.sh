d0becb2d882b7274e6ae4e67d7a8b9c19c32397a
ssimms <ssimms@lbl.gov>
2023-07-17 17:40:55 -0700

    draft qos scripts

---
+++ QoS_scripts/qos1_example_script.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# This is an example job script for running QoS test #1
+# executing IOR jobs with near simultaneity
+
+#SBATCH --job-name=QOS_job
+
+# --nodes=X where X is the total number of compute nodes that will be
+# used for all five jobs at once. Use the number of nodes that you needed
+# to saturate and multiply by 5.
+
+#SBATCH --nodes=25
+#SBATCH --time=1:00:00
+#SBATCH --constraint=cpu
+
+# -N is the number of compute nodes per job -n is the number of MPI threads
+# and should correspond with the numTasks variable in the IOR script
+
+# TWO JOBS
+
+echo "TWO JOBS"
+
+# Here's where you would set the read and write QoS for directory_1
+# and directory_2
+
+cd $SCRATCH/directory_1
+srun -N 5 -n 50 ior-3.3.0/src/ior -f input.N10/load1-posix-NtoN.ior &
+cd $SCRATCH/directory_2
+srun -N 5 -n 50 ior-3.3.0/src/ior -f input.N10/load1-posix-NtoN.ior &
+
+wait
+
+echo "THREE JOBS"
+
+# Here's where you would set the read and write Qos for directory_1, directory_2,
+# and directory_3
+
+cd $SCRATCH/directory_1
+srun -N 5 -n 50 ior-3.3.0/src/ior -f input.N10/load1-posix-NtoN.ior &
+cd $SCRATCH/directory_2
+srun -N 5 -n 50 ior-3.3.0/src/ior -f input.N10/load1-posix-NtoN.ior &
+cd $SCRATCH/directory_3
+srun -N 5 -n 50 ior-3.3.0/src/ior -f input.N10/load1-posix-NtoN.ior &
+
+wait
+
+echo "FOUR JOBS"
+
+# Here's where you would set the read and write Qos for directory_1, directory_2,
+# directory_3, and directory_4
+
+cd $SCRATCH/directory_1
+srun -N 5 -n 50 ior-3.3.0/src/ior -f input.N10/load1-posix-NtoN.ior &
+cd $SCRATCH/directory_2
+srun -N 5 -n 50 ior-3.3.0/src/ior -f input.N10/load1-posix-NtoN.ior &
+cd $SCRATCH/directory_3
+srun -N 5 -n 50 ior-3.3.0/src/ior -f input.N10/load1-posix-NtoN.ior &
+cd $SCRATCH/directory_4
+srun -N 5 -n 50 ior-3.3.0/src/ior -f input.N10/load1-posix-NtoN.ior &
+
+wait
+
+echo "FIVE JOBS"
+
+# Here's where you would set the read and write Qos for directory_1, directory_2,
+# directory_3, directory_4, and directory_5
+
+cd $SCRATCH/directory_1
+srun -N 5 -n 50 ior-3.3.0/src/ior -f input.N10/load1-posix-NtoN.ior &
+cd $SCRATCH/directory_2
+srun -N 5 -n 50 ior-3.3.0/src/ior -f input.N10/load1-posix-NtoN.ior &
+cd $SCRATCH/directory_3
+srun -N 5 -n 50 ior-3.3.0/src/ior -f input.N10/load1-posix-NtoN.ior &
+cd $SCRATCH/directory_4
+srun -N 5 -n 50 ior-3.3.0/src/ior -f input.N10/load1-posix-NtoN.ior &
+cd $SCRATCH/directory_5
+srun -N 5 -n 50 ior-3.3.0/src/ior -f input.N10/load1-posix-NtoN.ior &
+
+wait
